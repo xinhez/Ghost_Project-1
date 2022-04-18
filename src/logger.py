896aa8918f50e10a16458971e5792756f7bc2828
Xinhe Zhang <xinhezhang@g.harvard.edu>
2022-04-18 14:16:19 -0400

    Rename logging method.

--- src/logger.py
+++ src/logger.py
@@ -8,7 +8,7 @@ class Logger:
         self.save_log_path = save_log_path
         self.verbose = verbose
 
-    def print_or_save(self, message):
+    def log_message(self, message):
         if self.verbose:
             print(message)
         if self.save_log_path is not None:
@@ -17,14 +17,14 @@ class Logger:
             file.close()
 
     def log_losses(self, losses):
-        self.print_or_save(f"{self.tab}Losses")
+        self.log_message(f"{self.tab}Losses")
         messages = [
             f"{self.tab}{self.tab}{key}: {losses[key]}" for key in sorted(losses.keys())
         ]
-        self.print_or_save("\n".join(messages))
+        self.log_message("\n".join(messages))
 
     def log_metrics(self, metrics):
-        self.print_or_save(f"{self.tab}Metrics")
+        self.log_message(f"{self.tab}Metrics")
         messages = []
         for key in sorted(metrics.keys()):
             if isinstance(metrics[key], list):
@@ -38,20 +38,20 @@ class Logger:
                 messages.append(header + metric)
             else:
                 messages.append(f"{self.tab}{self.tab}{key}: {metrics[key]}")
-        self.print_or_save("\n".join(messages))
+        self.log_message("\n".join(messages))
 
     def log_epoch_start(self, epoch, n_epoch):
         message = f"\n{self.tab}(Epoch {epoch} / {n_epoch})"
-        self.print_or_save(message)
+        self.log_message(message)
 
     def log_save_model(self, name):
         message = f"{self.tab}{self.tab}{self.tab}Saving model to {name}"
-        self.print_or_save(message)
+        self.log_message(message)
 
     def log_schedule_start(self, schedule):
         message = f"{self.tab}========== Schedule {schedule.order}: {schedule.name} =========="
-        self.print_or_save(message)
+        self.log_message(message)
 
     def log_method_start(self, method, task=None):
         message = f"{method.upper()} {''if task is None else 'Task:'} {'' if task is None else task}"
-        self.print_or_save(message)
+        self.log_message(message)
